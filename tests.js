5ebc1abed2da29b9df6a5c04fb66d86156cc3ab9
Josh Marchand <josh.r.marchand@gmail.com>
2020-02-10 14:17:38 -0600

    3

--- tests.js
+++ tests.js
@@ -18,4 +18,10 @@ describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
-})
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return a \'Hello, Jane!\' when called', function () {
+        expect(typeof sayHello('Jane')).toBe('Hello, Jane!');
+    });
+});
